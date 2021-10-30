9e9d76585231bb48e6b43e8129f8d6213b92f733
sanskrutibadhe7@gmail.com <sanskrutibadhe7@gmail.com>
2021-10-30 12:30:41 +0530

    periodic table component

--- mindspark/src/App.js
+++ mindspark/src/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './CSS/bootstrap.min.css';
+import PeriodicTable from "./components/PeriodicTable"
 // import './CSS/bootstrap.bundle.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Periodic Table of Elements</h1>
+      <small> with React + CSS Grid</small>
+      <PeriodicTable />
+    </div>
+  );
 }
 
 export default App;

683d2cd27388e3d0ae71777d645580f770d060a3
Mayur <mayur1311gt@gmail.com>
2021-10-30 23:27:58 +0530

    element component and periodic elements done

--- mindspark/src/components/Element.js
+++ mindspark/src/components/Element.js
@@ -1,33 +1,42 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Element() {
 
     const [elem, setelem] = useState(JSON.parse(localStorage.getItem("el")))
-    // console.log(JSON.parse(localStorage.getItem("el")))
+    console.log(JSON.parse(localStorage.getItem("el")))
 
     return (
         <div>
             <div className="container">
-                <h1 className="text-center">{elem.name}</h1>
-                <iframe src="https://giphy.com/embed/ZZqrogB7mIcppuwJc7" width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZZqrogB7mIcppuwJc7">via GIPHY</a></p>
-                <div className="row d-flex justify-content-spacebetween">
-                    <div className="col-6">
+                <div className="row my-3">
+                    <div className="col-4">
+                        <iframe src="https://giphy.com/embed/ZZqrogB7mIcppuwJc7" width="480" height="480" frameBorder="0" className="giphy-embed noHover" allowFullScreen></iframe>
+                    </div>
+                    <div className="col-8">
+                        <h1>{elem.symbol}</h1>
+                        <h5>{elem.name}</h5>
+                        
+                    </div>
+                </div>
+                <div className="row d-flex justify-content-between">
+                    <div className="col-4">
                         <div className="card cardwidth">
-                            <img src="..." className="card-img-top" alt="..."/>
+                            <img src="https://www.scienceabc.com/wp-content/uploads/ext-www.scienceabc.com/wp-content/uploads/2017/05/Photoelectric-Effect.jpg-.jpg" className="card-img-top" alt="..."/>
                             <div class ="card-body">
-                            <h5 class ="card-title">Card title</h5>
+                            <h5 class ="card-title">Photoelectric Effect</h5>
                             <p class ="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/" class ="btn btn-primary">Go somewhere</a>
+                            <Link to="/photoelectric" class ="btn btn-primary">Begin</Link>
                             </div>
                         </div>
                     </div>
-                    <div className="col-6">
+                    <div className="col-4">
                         <div className="card cardwidth">
-                            <img src="..." className="card-img-top" alt="..."/>
+                            <img src="https://cdn1.byjus.com/wp-content/uploads/2016/01/Melting-and-Boiling-Point2.png" className="card-img-top" alt="..."/>
                             <div class ="card-body">
-                            <h5 class ="card-title">Card title</h5>
+                            <h5 class ="card-title">Melting and Boiling point of elements</h5>
                             <p class ="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class ="btn btn-primary">Go somewhere</a>
+                            <Link to="/sample" class ="btn btn-primary">Begin</Link>
                             </div>
                         </div>
                     </div>

cd85d31bd3a21a343b2ce74fa22b19952d65acdd
Mayur <mayur1311gt@gmail.com>
2021-10-30 22:58:51 +0530

    element component

--- mindspark/src/components/Element.js
+++ mindspark/src/components/Element.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Element() {
+
+    const [elem, setelem] = useState(JSON.parse(localStorage.getItem("el")))
+    // console.log(JSON.parse(localStorage.getItem("el")))
+
     return (
         <div>
             <div className="container">
-                <h1 className="text-center">Iridium</h1>
-                <iframe src="https://giphy.com/embed/ZZqrogB7mIcppuwJc7" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZZqrogB7mIcppuwJc7">via GIPHY</a></p>
+                <h1 className="text-center">{elem.name}</h1>
+                <iframe src="https://giphy.com/embed/ZZqrogB7mIcppuwJc7" width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZZqrogB7mIcppuwJc7">via GIPHY</a></p>
+                <div className="row d-flex justify-content-spacebetween">
+                    <div className="col-6">
+                        <div className="card cardwidth">
+                            <img src="..." className="card-img-top" alt="..."/>
+                            <div class ="card-body">
+                            <h5 class ="card-title">Card title</h5>
+                            <p class ="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="/" class ="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="card cardwidth">
+                            <img src="..." className="card-img-top" alt="..."/>
+                            <div class ="card-body">
+                            <h5 class ="card-title">Card title</h5>
+                            <p class ="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class ="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )

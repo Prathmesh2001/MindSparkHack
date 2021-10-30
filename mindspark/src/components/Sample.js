21872f1c50f4c6b5f000a239818fa7d986c50656
sanskrutibadhe7@gmail.com <sanskrutibadhe7@gmail.com>
2021-10-30 17:36:33 +0530

    trial1

--- mindspark/src/components/Sample.js
+++ mindspark/src/components/Sample.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import data from "../PeriodicTableJSON.json"
 
 export default function Sample() {
     return (
-    <div class="flame-wrapper">
-     <div class="flame red"></div>
-     <div class="flame orange"></div>
-     <div class="flame gold"></div>
-     <div class="flame white"></div>
+        <div class="grid-container">
+        <div class="wrapper">
+        <div class="flame-wrapper">
+            <div class="flame red"></div>
+            <div class="flame orange"></div>
+            <div class="flame gold"></div>
+            <div class="flame white"></div>
+            <div class="base blue"></div>
+            <div class="base black"></div>
+        </div>
+        </div>
+        <div class="cup">
     </div>
+    
+    </div>
+    
     )
 }

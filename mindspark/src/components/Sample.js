cc9ad4e17e40a9a0a231e51be89431abaca708ac
Prathmesh2001 <prathmeshw1102@gmail.com>
2021-10-31 03:13:47 +0530

    CSS to Photo electric

--- mindspark/src/components/Sample.js
+++ mindspark/src/components/Sample.js
@@ -6,20 +6,20 @@ export default function Sample() {
     return (
         <div>
 
-            <div class="body">
+            <div className="body">
 
-                <div class="wrapper">
-                    <div class="flame-wrapper">
-                        <div class="flame red"></div>
-                        <div class="flame orange"></div>
-                        <div class="flame gold"></div>
-                        <div class="flame white"></div>
-                        <div class="base blue"></div>
-                        <div class="base black"></div>
+                <div className="wrapper center-object-vertical">
+                    <div className="flame-wrapper">
+                        <div className="flame red"></div>
+                        <div className="flame orange"></div>
+                        <div className="flame gold"></div>
+                        <div className="flame white"></div>
+                        <div className="base blue"></div>
+                        <div className="base black"></div>
                     </div>
                 </div>
-                <div className="box-container center-object-vertical my-5">
-                    {/* <div class="cup"> </div> */}
+                {/* <div className="box-container center-object-vertical my-5">
+                    <div className="cup"> </div>
                     <div className="flask">
                         <img src="/static/images/boiling-water.gif" alt="round flask" />
                     </div>
@@ -27,10 +27,10 @@ export default function Sample() {
                         <img src="/static/images/fire.gif" alt="blue fire" />
                     </div>
 
-                </div>
+                </div> */}
 
             </div>
-            <div class="cup">
+            <div className="cup">
             </div>
 
             <div className="body1"><CounterOne /><br /></div>

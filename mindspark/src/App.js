42c381a69e3103b12385c8f295a1fe4e5d9ffcbc
sanskrutibadhe7@gmail.com <sanskrutibadhe7@gmail.com>
2021-10-30 12:52:08 +0530

    Merge1

--- mindspark/src/App.js
+++ mindspark/src/App.js
@@ -5,11 +5,11 @@ import './CSS/bootstrap.min.css';
 import PeriodicTable from "./components/PeriodicTable"
 // import './CSS/bootstrap.bundle.js';
 
+
 function App() {
   return (
     <div className="App">
       <h1>Periodic Table of Elements</h1>
-      <small> with React + CSS Grid</small>
       <PeriodicTable />
     </div>
   );

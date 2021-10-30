0017224e9d0f74f68ef603e5fb6b323adbdb229e
Mayur <mayur1311gt@gmail.com>
2021-10-30 12:30:25 +0530

    app css deleted

--- mindspark/src/components/Photoelectric.js
+++ mindspark/src/components/Photoelectric.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Photoelectric() {
     return (
-        <div>
-            Photoelectric
+        <div className='container'>
+            <h1 className="text-center">Photoelectric Effect</h1>
+            <div className="row">
+                <div className="col-4">
+                    m
+                </div>
+                <div className="col-8">
+                    n
+                </div>
+            </div>
         </div>
     )
 }

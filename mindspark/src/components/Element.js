d0d0d1a82dd766170514ae1f135f01316d7a4e5f
Mayur <mayur1311gt@gmail.com>
2021-10-30 18:01:10 +0530

    Added element and photoelectric component

---
+++ mindspark/src/components/Element.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Element() {
+    return (
+        <div>
+            <div className="container">
+                <h1 className="text-center">Iridium</h1>
+                <iframe src="https://giphy.com/embed/ZZqrogB7mIcppuwJc7" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZZqrogB7mIcppuwJc7">via GIPHY</a></p>
+            </div>
+        </div>
+    )
+}

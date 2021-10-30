8329fe982ea638de894775d4b0b160c4e9a3adc4
Prathmesh2001 <prathmeshw1102@gmail.com>
2021-10-30 13:35:18 +0530

    react-router-dom added

--- mindspark/src/App.js
+++ mindspark/src/App.js
@@ -1,16 +1,40 @@
 
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+} from "react-router-dom";
+
 import './CSS/styles.css';
 import './CSS/bootstrap.min.css';
 import PeriodicTable from "./components/PeriodicTable"
+import Photoelectric from './components/Sample';
+import Home from './components/Home';
 // import './CSS/bootstrap.bundle.js';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Periodic Table of Elements</h1>
-      <PeriodicTable />
+    
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/photoElectric">
+            <Photoelectric />
+          </Route>
+          <Route exact path="/periodicTable">
+            <h1>Periodic Table of Elements</h1>
+            <PeriodicTable />
+          </Route>
+        </Switch>
+      </Router>
+
+
     </div>
   );
 }

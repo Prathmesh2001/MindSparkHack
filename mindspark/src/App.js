d6f87b41253fde3f902aa63326fd62568e9c0eee
Prathmesh2001 <prathmeshw1102@gmail.com>
2021-10-30 16:51:42 +0530

    bonding page add

--- mindspark/src/App.js
+++ mindspark/src/App.js
@@ -12,25 +12,34 @@ import './CSS/bootstrap.min.css';
 import PeriodicTable from "./components/PeriodicTable"
 import Photoelectric from './components/Sample';
 import Home from './components/Home';
+import Bonding from './components/Bonding';
 // import './CSS/bootstrap.bundle.js';
 
 
 function App() {
   return (
     <div className="App">
-    
+      
       <Router>
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-          <Route exact path="/photoElectric">
-            <Photoelectric />
-          </Route>
+
           <Route exact path="/periodicTable">
             <h1>Periodic Table of Elements</h1>
             <PeriodicTable />
           </Route>
+
+          <Route exact path="/photoElectric">
+            <Photoelectric />
+          </Route>
+
+          <Route exact path="/bonding">
+            <Bonding/>
+          </Route>
+
+          
         </Switch>
       </Router>
 
